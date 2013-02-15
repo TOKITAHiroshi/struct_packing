0b8776ce15ac234929963192137e1c55556eedbb
TOKITA Hiroshi <tokita.hiroshi+github@gmail.com>
2013-02-15 09:46:34 +0900

    Correct wrong class-variable genaration. Create class variable "@@byte_format" for each inherited modules.

--- lib/struct_packing/base.rb
+++ lib/struct_packing/base.rb
@@ -3,15 +3,20 @@ module StructPacking
   
   private
   
+  # Common defines for Packable and Unpackable 
   module Base
+    
+    private
     
     def self.included(base)
       base.extend ClassMethods
-      if not base.class_variable_defined?(:@@byte_format)
-        base.class_eval("@@byte_format = {}")
-      end
     end
   
+    public
+    
+    # Get structure format string used in packing this object.
+    #
+    # This method work as just wrapper to same name class-method. 
     def byte_format
       self.class.byte_format
     end
@@ -19,11 +24,10 @@ module StructPacking
     module ClassMethods
       
       private
-  
+
       def append_define(name, type)
         count = 0
         byte_format.values.collect {|v| count += v[:size] }
-  
         size = Util.size_of( type )
         
         byte_format[name] = {:type=>type, :start=>count, :size=>size}
@@ -45,13 +49,25 @@ module StructPacking
         params
       end
       
-      public
       
+      def check_vardef
+        if not self.class_variable_defined?(:@@byte_format)
+          class_eval("@@byte_format = {}")
+        end
+      end
+      
+      public
+
+      # Get structure format string used in packing object of this class.
       def byte_format
+        check_vardef # TODO Find more good way!
+        
         self.class_variable_get(:@@byte_format)
       end
       
+      # Set struct format string for this class.
       def byte_format=(arg)
+        check_vardef
         
         if arg.is_a?(String)
           params = parse_format_text(arg)
@@ -61,7 +77,9 @@ module StructPacking
         
         params.keys.each do |f|
           append_define(f, params[f])
-        end   
+        end
+
+        true
       end
     end
   end
